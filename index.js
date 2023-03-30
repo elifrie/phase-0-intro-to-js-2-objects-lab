a2ac2723105e524900694331152a74956c19e028
Eli Friedlander <eli.friedlander@gmail.com>
2023-03-30 11:51:30 -0400

    By yourself this time

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: "Eli Friedlander",
+    streetAddress: "85 Attorney"
+}
+function updateEmployeeWithKeyAndValue (employee,key,value) {
+    const newEmployee = { ...employee };
+
+  newEmployee[key] = value;
+
+  return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee,key,value) {
+    employee[key] = value;
+
+    return employee
+}
+
+function deleteFromEmployeeByKey (employee,key) {
+    const employeeNoKey = {...employee};
+    delete employeeNoKey[key];
+    return employeeNoKey
+}
+function destructivelyDeleteFromEmployeeByKey (employee,key) {
+    delete employee[key];
+    return employee
+}
